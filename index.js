989a98c01385799d07f16625e31fea0b4e2e291b
ShaunZap <sjbzappy@gmail.com>
2024-01-30 09:28:41 +0530

    optimized

--- index.js
+++ index.js
@@ -5,20 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
 async function getJokes() {
     const currentJokeType = document.getElementById("selectType").value;
     let response, data;
+    response = await fetch(`https://v2.jokeapi.dev/joke/${currentJokeType}?type=twopart`);
 
-    if (currentJokeType === "Dark") {
-        response = await fetch("https://v2.jokeapi.dev/joke/Dark?type=twopart");
-    } else if (currentJokeType === "Pun") {
-        response = await fetch("https://v2.jokeapi.dev/joke/Pun?type=twopart");
-    } else if (currentJokeType === "Programming") {
-        response = await fetch("https://v2.jokeapi.dev/joke/Programming?type=twopart");
-    } else if (currentJokeType === "Spooky") {
-        response = await fetch("https://v2.jokeapi.dev/joke/Spooky?type=twopart");
-    } else if (currentJokeType === "Christmas") {
-        response = await fetch("https://v2.jokeapi.dev/joke/Christmas?type=twopart");
-    } else if (currentJokeType == "Random"){
-        response = await fetch("https://v2.jokeapi.dev/joke/Miscellaneous?type=twopart");
-    }
     data = await response.json();
     console.log(data);
     document.querySelector(".setup").innerHTML = data.setup;
